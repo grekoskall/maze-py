ae8b753232a757a823bd9308ba0d91bdcd75e065
grekoskall <grekoskall@gmail.com>
2025-05-30 23:51:40 +0300

    Cell class start

---
+++ cell.py
@@ -0,0 +1,16 @@
+
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
+        self.__win = window
+
+    def draw(self):
+
+
